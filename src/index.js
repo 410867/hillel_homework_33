5f2fbf5e35589073adbfed73c58bbe4ec73022e5
410867 <oshchyp123@gmail.com>
2023-03-06 11:23:43 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { users } from './users';
 const fastify = Fastify({
   logger: true,
 });
+
 fastify.register(import('@fastify/cors'));
 fastify.register(import('@fastify/multipart'), {
   addToBody: true,
